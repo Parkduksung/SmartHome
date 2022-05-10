3507235ba4bb2101583c35c45bd9f5cb96e2c1c1
Parkduksung <duksung1234@naver.com>
2022-05-11 00:50:51 +0900

    [feature] api 수정.

--- notcompose/src/main/java/com/example/notcompose/data/SensorApi.kt
+++ notcompose/src/main/java/com/example/notcompose/data/SensorApi.kt
@@ -1,11 +1,26 @@
 package com.example.notcompose.data
 
 import com.example.notcompose.data.model.SensorResponse
+import com.google.gson.annotations.SerializedName
 import retrofit2.Call
 import retrofit2.http.GET
+import retrofit2.http.POST
+import retrofit2.http.Query
 
 interface SensorApi {
 
     @GET("window.php")
     fun getSensorData(): Call<SensorResponse>
+
+    @POST("http://115.145.175.57:10099/users")
+    fun getResult(
+        @Query("username") username: String
+    ): Call<LoginResult>
+
 }
+
+
+data class LoginResult(
+    @SerializedName("success")
+    val success: Boolean
+)
